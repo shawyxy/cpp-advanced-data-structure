7184632a5d9630a2ed5bd01e87551b6f8fc20861
shawyxy <9inghuan@gmail.com>
2022-09-04 23:34:22 +0800

    test

---
+++ 22_9_4_test/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+//
+//int main() {
+//    int a = 2;
+//    int b = 3333;
+//    printf("%2d\n", a);
+//    printf("%d\n", b);
+//
+//}
+//using namespace std;
+//int main()
+//{
+//    string str("hello");
+//    string::iterator it = str.begin();
+//    while(it != str.end())
+//    {
+//        cout << *it << " ";
+//        it++;
+//    }
+//    return 0;
+//}
