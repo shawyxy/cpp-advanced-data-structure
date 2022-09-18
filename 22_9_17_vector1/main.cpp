a3027a4db91b19649dbd6ad34ce97140715685c9
shawyxy <9inghuan@gmail.com>
2022-09-18 21:59:26 +0800

    vector1

---
+++ 22_9_17_vector1/main.cpp
@@ -0,0 +1,19 @@
+#include "Myvector.h"
+
+//void test1()
+//{
+////    xy::vector<int> v;
+////    cout << v.size();
+//    xy::vector<int> v;
+//    //v.push_back();
+////    for(auto& a v)
+////    {
+////        cout << *a << " ";
+////    }
+////    cout << endl;
+//}
+int main()
+{
+
+    return 0;
+}
