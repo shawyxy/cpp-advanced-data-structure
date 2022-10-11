03dd0a86247c81b33d3bb7c2d08b2afe628fe686
shawyxy <9inghuan@gmail.com>
2022-10-11 23:51:13 +0800

    test

---
+++ 22_10_11_test/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//
+//int main() {
+//    int d1[] = { 1, 2 ,3};
+//    int d2[] = { 2, 3, 4};
+//    int* arr[] = { d1, d2};
+//    int arr1[] = {d1, d2};
+//    return 0;
+//}
