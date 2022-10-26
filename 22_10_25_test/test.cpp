61156bbe47f136797b2adafa6d378350c3ea39e2
shawyxy <9inghuan@gmail.com>
2022-10-26 20:45:02 +0800

    test

---
+++ 22_10_25_test/test.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+// class A
+// {
+//     public:
+//     void f()
+//     {
+//         cout << "A" << endl;
+//     }
+// };
+// class B : public A{
+// public:
+// virtual void f()
+// {
+//     cout << "B" << endl;
+// }
+// };
+struct A
+{
+    /* data */
+    void foo()
+    {
+        cout << " foo " << endl;
+    }
+    virtual void bar()
+    {
+        cout << " bar " << endl;
+    }
+    A()
+    {
+        bar();
+    }
+};
+
+struct B
+{
+    /* data */
+    void foo()
+    {
+        cout << "b_foo" << endl;
+    }
+    void bar()
+    {
+        cout << "b_bar" << endl;
+    }
+};
+
+
+int main()
+{
+//     A* a = new B;
+//     a->f();
+//     delete a;
+    A* p  = new B;
+    p->foo();
+    p->bar();
+    return 0;
+}
