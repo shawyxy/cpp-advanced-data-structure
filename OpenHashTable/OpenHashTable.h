3d619bedd599a772f32ae090deaca94678d3839c
shawyxy <9inghuan@gmail.com>
2022-12-05 21:27:58 +0800

    补全哈希桶实现哈希表

--- OpenHashTable/OpenHashTable.h
+++ OpenHashTable/OpenHashTable.h
@@ -1,5 +1,16 @@
 #pragma once
 
+static const int __my__num_primes = 28;
+static const unsigned long MyPrimeList[__my__num_primes] =
+        {
+                53,         97,         193,       389,       769,
+                1543,       3079,       6151,      12289,     24593,
+                49157,      98317,      196613,    393241,    786433,
+                1572869,    3145739,    6291469,   12582917,  25165843,
+                50331653,   100663319,  201326611, 402653189, 805306457,
+                1610612741, 3221225473, 4294967291
+        };
+
 template<class K, class V>
 struct HashNode
 {
@@ -10,8 +21,33 @@ struct HashNode
         , _next(nullptr)
     {}
 };
-
-template<class K, class V>
+// 仿函数
+template<class K>
+struct HashFunc
+{
+    size_t operator()(const K& key)
+    {
+        return (size_t)key;
+    }
+};
+// BKDRHash算法
+// 字符串转整型
+template<>
+struct HashFunc<string>
+{
+    size_t operator()(const string& key)
+    {
+        size_t ret = 0;
+        for(auto& e : key)
+        {
+            ret *= 131;
+            ret += e;
+        }
+        return ret;
+    }
+};
+// 哈希表
+template<class K, class V, class Hash = HashFunc<K>>
 class HashTable
 {
     typedef HashNode<K, V> Node;
@@ -21,11 +57,11 @@ public:
     {
         for (size_t i = 0; i < _table.size(); i++)
         {
-            Node* cur = _table[i];
-            while (cur)
+            Node* cur = _table[i];         // 得到每个桶的首结点地址
+            while (cur)                    // 释放桶中每个结点的资源
             {
                 Node* next = cur->_next;
-                free(cur);
+                delete cur;
                 cur = next;
             }
             _table[i] = nullptr;
@@ -38,8 +74,8 @@ public:
         {
             return nullptr;
         }
-
-        size_t pos = key % _table.size();   // 得到下标值
+        Hash hash;
+        size_t pos = hash(key) % _table.size();   // 得到下标值
         HashNode<K, V>* cur = _table[pos];  // 找到哈希桶首地址
         while (cur)                         // 遍历链表
         {
@@ -54,6 +90,7 @@ public:
     // 插入函数
     bool Insert(const pair<K, V>& kv)
     {
+        Hash hash;
         if(Find(kv.first) != nullptr) // 元素已经存在
         {
             return false;
@@ -63,7 +100,8 @@ public:
         if(_size == _table.size())
         {
             size_t oldSize = _table.size();
-            size_t newSize = oldSize == 0 ? 10 : 2 * oldSize;
+            //size_t newSize = oldSize == 0 ? 10 : 2 * oldSize;
+            size_t newSize = GetNextPrime(oldSize);
             vector<Node*> newTable;                         // 建立新表
             newTable.resize(newSize);                       // 扩容
             for(size_t i = 0; i < oldSize; i++)             // 转移数据
@@ -73,7 +111,7 @@ public:
                 {
                     Node* next = cur->_next;
 
-                    size_t hashi = cur->_kv.first % newSize;// 新下标值
+                    size_t hashi = hash(cur->_kv.first) % newSize;// 新下标值
                     cur->_next = newTable[hashi];           // 重新链接
                     newTable[hashi] = cur;
 
@@ -81,11 +119,11 @@ public:
                 }
                 _table[i] = nullptr;
             }
-            _table.swap(newTable);                          // 替换新哈希表
+            _table.swap(newTable);                       // 替换新哈希表
         }
 
         // 头插元素
-        size_t hashi = kv.first % _table.size();
+        size_t hashi = hash(kv.first) % _table.size();
         Node* newnode = new Node(kv);
         newnode->_next = _table[hashi];
         _table[hashi] = newnode;
@@ -96,7 +134,8 @@ public:
     // 删除函数
     bool Erase(const K& key)
     {
-        size_t pos = key % _table.size();       // 得到key值对应的哈希桶下标
+        Hash hash;
+        size_t pos = hash(key) % _table.size(); // 得到key值对应的哈希桶下标
         Node* prev = nullptr;
         Node* cur = _table[pos];
         while (cur)
@@ -121,24 +160,100 @@ public:
         }
         return false;
     }
+    size_t Size()
+    {
+        return _size;
+    }
+
+    // 哈希表的长度
+    size_t TablesSize()
+    {
+        return _table.size();
+    }
+
+    // 哈希桶的个数
+    size_t BucketNum()
+    {
+        size_t num = 0;
+        for (size_t i = 0; i < _table.size(); i++)
+        {
+            if (_table[i])
+            {
+                num++;
+            }
+        }
+
+        return num;
+    }
+    // 最长桶的长度
+    size_t MaxBucketLenth()
+    {
+        size_t maxLen = 0;
+        for (size_t i = 0; i < _table.size(); i++)
+        {
+            size_t len = 0;
+            Node* cur = _table[i];
+            while (cur)
+            {
+                ++len;
+                cur = cur->_next;
+            }
+
+            if (len > maxLen)
+            {
+                maxLen = len;
+            }
+        }
+
+        return maxLen;
+    }
     // 打印函数
     void Print()
     {
-        for (size_t i = 0; i < _table.size(); ++i)
+        for (size_t i = 0; i < _table.size(); i++)
         {
             Node* cur = _table[i];
             while(cur)
             {
-                printf("[%d:%d]", i, cur->_kv.first);
+                //printf("[%d:%d]", i, cur->_kv.first); // 只适用于int型key值打印
+                cout << "[" <<  i << ":" << cur->_kv.first << "]" << " "; // 使用于任何值打印
                 if(cur->_next)
                     printf("->");
 
                 cur = cur->_next;
             }
-            if(_table[i] == nullptr)
-                printf("[%d:*] ", i);
-            cout << endl;
+            //if(_table[i] == nullptr)
+                //printf("[%d:*] ", i);
+            //cout << endl;
         }
+    }
+    // 打印函数
+    void Print2()
+    {
+        for (size_t i = 0; i < _table.size(); i++)
+        {
+            Node* cur = _table[i];
+            while(cur)
+            {
+                cout << "[" <<  cur->_kv.first << ":" << cur->_kv.second << "]" << " ";
+                if(cur->_next)
+                    printf("->");
+
+                cur = cur->_next;
+            }
+        }
+    }
+    // 获取新哈希表素数长度
+    size_t GetNextPrime(size_t prime)
+    {
+        const int PRIMECOUNT = 28;
+        size_t i = 0;
+        for (i = 0; i < PRIMECOUNT; i++)
+        {
+            if (MyPrimeList[i] > prime)
+                return MyPrimeList[i];
+        }
+        return MyPrimeList[i];
     }
 private:
     vector<Node*> _table;       // 哈希表
--- OpenHashTable/main.cpp
+++ OpenHashTable/main.cpp
@@ -20,7 +20,82 @@ void OpenHashTableTest1()
     cout << (ht.Find(26)->_kv.first) << endl;
 
 }
+void OpenHashTableTest2()
+{
+    int a[] = { 1, 11, 4, 15, 26, 7, 44,55,99,78 };
+    HashTable<int, int> ht;
+    for (auto e : a)
+    {
+        ht.Insert(make_pair(e, e));
+    }
+    ht.Print();
+
+    ht.Insert(make_pair(22,22));
+    cout << endl;
+
+    ht.Print();
+
+    ht.Erase(44);
+    ht.Erase(4);
+    cout << endl;
+
+
+    ht.Print();
+
+}
+
+void OpenHashTableTest3()
+{
+    string arr[] = { "苹果", "西瓜", "苹果", "西瓜", "苹果", "苹果", "西瓜", "苹果", "香蕉", "苹果", "香蕉" };
+
+    HashTable<string, int> countHT;
+    //HashTable<string, int>> countHT;
+    for (auto& str : arr)
+    {
+        auto ptr = countHT.Find(str);
+        if (ptr)
+        {
+            ptr->_kv.second++;
+        }
+        else
+        {
+            countHT.Insert(make_pair(str, 1));
+        }
+    }
+    countHT.Print2();
+}
+
+void OpenHashTableTest4()
+{
+
+    int n = 9000000;
+    vector<int> v;
+    v.reserve(n);
+    srand(time(0));
+    for (int i = 0; i < n; ++i)
+    {
+        //v.push_back(i);
+        v.push_back(rand() + i);    // 重复少
+        //v.push_back(rand());      // 重复多
+    }
+
+    size_t begin1 = clock();
+    HashTable<int, int> ht;
+    for (auto e : v)
+    {
+        ht.Insert(make_pair(e, e));
+    }
+    size_t end1 = clock();
+
+    cout << "数据个数:" << ht.Size() << endl;
+    cout << "表的长度:" << ht.TablesSize() << endl;
+    cout << "桶的个数:" << ht.BucketNum() << endl;
+    cout << "平均每个桶的长度:" << (double)ht.Size() / (double)ht.BucketNum() << endl;
+    cout << "最长的桶的长度:" << ht.MaxBucketLenth() << endl;
+    cout << "负载因子:" << (double)ht.Size() / (double)ht.TablesSize() << endl;
+}
+
 int main()
 {
-    OpenHashTableTest1();
+    OpenHashTableTest4();
 }
