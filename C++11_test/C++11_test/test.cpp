ab34cf865e13eda6103ff3a93213867fafac45ea
shawyxy <9inghuan@gmail.com>
2022-12-22 23:29:05 +0800

    整数划分

--- C++11_test/C++11_test/test.cpp
+++ C++11_test/C++11_test/test.cpp
@@ -1,8 +1,86 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <iostream>
+﻿//#include<iostream>
+//#include <algorithm>
+//using namespace std;
+//const int N = 1024;
+//int dp[N][N] = { 0 };
+//int func(int n)
+//{
+//    for (int i = 1; i <= n; i++)
+//    {
+//        for (int j = 1; j <= n; j++)
+//        {
+//            if (i == 1 || j == 1)
+//                dp[i][j] = 1;
+//            for(int k = 0; k * i <= j; k++)
+//                dp[i][j] = max(dp[i][j], dp[i - 1][j - i * k] + i * k);
+//        }
+//    }
+//    return dp[n][n];
+//}
+//int main()
+//{
+//    int n = 0;   
+//    while (cin >> n) 
+//    {
+//        cout << func(n) << endl;
+//    }
+//    return 0;
+//}
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//const int N = 64;
+//int n;
+//int dp[N][N];
+//int func(int n)
+//{
+//    dp[0][0] = 1;
+//    for (int i = 0; i <= n; i++)
+//        for (int j = 1; j <= n; j++)
+//        {
+//            dp[i][j] = dp[i][j - 1];
+//            if (i >= j) dp[i][j] += dp[i - j][j];
+//        }
+//    return dp[n][n];
+//}
+//int main()
+//{
+//    while (cin >> n)
+//    {
+//        cout << func(n) << endl;
+//        for (int i = 0; i <= n; i++)
+//        {
+//            for (int j = 0; j <= n; j++)
+//            {
+//                cout << dp[i][j] << " ";
+//            }
+//            cout << endl;
+//        }
+//    }
+//    return 0;
+//}
+#include<iostream>
+#include<algorithm>
+#include <string>
 using namespace std;
-int main()
+
+const int N = 64;
+int dp[N];
+string ret = "";
+int main() 
 {
-	cout << "hello" << endl; 
+	int n = 0;
+	cin >> n;
+	dp[0] = 1;
+	for (int i = 1; i <= n; i++)
+		for (int j = i; j <= n; j++)
+		{
+			dp[j] = (dp[j] + dp[j - i]);
+			if()
+		}
+	cout << dp[n] << endl;
 	return 0;
 }
+
