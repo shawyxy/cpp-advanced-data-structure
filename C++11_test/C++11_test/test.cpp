0227c41a487c84c3bd5da4e7bbbf273303f05945
shawyxy <9inghuan@gmail.com>
2022-12-23 23:12:37 +0800

    回溯打印

--- C++11_test/C++11_test/test.cpp
+++ C++11_test/C++11_test/test.cpp
@@ -1,8 +1,10 @@
-﻿//#include<iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<iostream>
 //#include <algorithm>
 //using namespace std;
 //const int N = 1024;
-//int dp[N][N] = { 0 };
+//int dp[N][N];
 //int func(int n)
 //{
 //    for (int i = 1; i <= n; i++)
@@ -11,8 +13,10 @@
 //        {
 //            if (i == 1 || j == 1)
 //                dp[i][j] = 1;
-//            for(int k = 0; k * i <= j; k++)
-//                dp[i][j] = max(dp[i][j], dp[i - 1][j - i * k] + i * k);
+//            for (int k = 0; k * i <= j; k++)
+//            {
+//
+//            }
 //        }
 //    }
 //    return dp[n][n];
@@ -61,26 +65,96 @@
 //    }
 //    return 0;
 //}
-#include<iostream>
-#include<algorithm>
-#include <string>
+#include <iostream>
 using namespace std;
 
 const int N = 64;
-int dp[N];
-string ret = "";
-int main() 
+int factor[N];
+
+int pos = 0;
+int n = 0;
+void DFSPrint(int index, int sum, int n) 
 {
-	int n = 0;
-	cin >> n;
-	dp[0] = 1;
-	for (int i = 1; i <= n; i++)
-		for (int j = i; j <= n; j++)
+	if (index == n || sum >= n) 
+	{
+		if (sum == n) 
 		{
-			dp[j] = (dp[j] + dp[j - i]);
-			if()
+			for (int i = 0; i < pos; i++)
+			{
+				if (i < pos - 1)
+					cout << factor[i] << "+";
+				else
+				{
+					cout << index << "=" << sum << endl;
+					if (index + 1 == n)
+						cout << n << "=" << n << endl;
+				}
+			}
 		}
-	cout << dp[n] << endl;
-	return 0;
+		return;
+	}
+
+	factor[pos++] = index;
+	DFSPrint(index, sum + index, n);
+
+	pos--;
+	DFSPrint(index + 1, sum, n);
 }
 
+
+int main()
+{
+	while (cin >> n)
+	{
+		int dp[N] = { 0 };
+		dp[0] = 1;
+
+		for (int i = 1; i <= n; i++)
+			for (int j = i; j <= n; j++)
+				dp[j] = dp[j] + dp[j - i];
+
+		cout << dp[n] << endl;
+
+		DFSPrint(1, 0, n);
+	}
+	return 0;
+}
+//
+//#include <iostream>
+//using namespace std;
+//const int N = 64;
+//int f[N][N];
+//
+//int func(int n)
+//{
+//	for (int i = 0; i <= n; i++) {
+//		f[i][0] = 1;					
+//	}
+//	for (int i = 1; i <= n; i++)
+//		for (int j = 1; j <= n; j++)
+//		{
+//			f[i][j] = f[i - 1][j] + f[i][j - i];
+//		}
+//	return f[n][n];
+//}
+//int main()
+//{
+//	int n = 0;
+//	int sum = 0;
+//	while (cin >> n)
+//	{
+//		cout << func(n) << endl;
+//		for (int i = 0; i <= n; i++)
+//		{
+//			sum = 0;
+//			for (int j = 0; j <= n; j++)
+//			{
+//				/*sum += j;
+//				if(sum <= j)*/
+//				cout << f[i][j] << " ";
+//			}
+//			cout << endl;
+//		}
+//	}
+//	return 0;
+//}
